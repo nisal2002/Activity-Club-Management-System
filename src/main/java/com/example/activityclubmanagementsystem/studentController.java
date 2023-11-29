0131c61dc4a7534193100739cd8c5f224de70355
thedinbanda02 <thedinbanda)2@gmail.com>
2023-11-29 16:02:24 +0530

    password validation

--- src/main/java/com/example/activityclubmanagementsystem/studentController.java
+++ src/main/java/com/example/activityclubmanagementsystem/studentController.java
@@ -1654,6 +1654,20 @@ public class studentController implements Initializable {
                 profileTxtRePwd.setStyle("-fx-border-color: white ; -fx-border-width: 2px ;");
             }
         }
+        if (profileTxtPwd.getLength() < 6 || profileTxtPwd.getLength() > 10)
+        {
+            completed=false;
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Information Dialog");
+            alert.setHeaderText(null);
+            alert.setContentText("Password must be between 6-10 Characters");
+
+            alert.showAndWait(); // Display the alert and wait for user interaction
+        }
+        else
+        {
+            completed=true;
+        }
         ArrayList<String> inputs = new ArrayList<>();
         inputs.add(profileTxtID.getText());
         inputs.add(FirstN);
